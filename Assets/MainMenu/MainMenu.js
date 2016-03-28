e58eea25910521471266d11bfe0f4d202944b424
wilowhite <thibaut.messe@yahoo.fr>
2016-03-28 15:47:14 +0200

    ezDG<
    dff

---
+++ Assets/MainMenu/MainMenu.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+function Start ()
+ 
+{
+
+}
+
+function Update ()
+
+ {
+
+}
+
+function StartGame ()
+
+ {
+  Application.LoadLevel("level1"); //la tu met le nom de la scène entre guillemet
+}
+
+function Exit ()
+
+ {
+ Application.Quit();
+}
+
+function Option ()
+
+ {
+
+}
