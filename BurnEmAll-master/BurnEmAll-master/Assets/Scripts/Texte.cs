26cf10c014df0ed9efa4deb45fa786f0350799ff
Yanato <mikadu355@yahoo.fr>
2016-04-27 21:08:27 +0200

    pas grand chose

--- BurnEmAll-master/BurnEmAll-master/Assets/Scripts/Texte.cs
+++ BurnEmAll-master/BurnEmAll-master/Assets/Scripts/Texte.cs
@@ -20,37 +20,29 @@ public class Texte : MonoBehaviour {
             Debug.Log(n);
                switch(n) {
                 case 1:
-				    texte.text = "Il n'y a pas si longtemps que ça \n ni très très loin non plus  \n un homme se réveillait en pleine nature  ";
+				    texte.text = "Celui-ci ne savait pas ce qu'il faisait là. \n En plus il était nue ! Mais sa c'est une autre histoire...";
                     break;
                 case 2:
-				    texte.text = "Celui-ci ne savait pas ce qu'il faisait ici \n En plus il était nue ! Mais sa c'est une autre histoire";
-                    break;
-                case 3:
-				    texte.text = "Alors qu'il reprenait ces esprits \n ceux-ci ce mirent à lui parler \n Ces voix tantôt douce tantôt puissante";
+				texte.text = "Alors qu'il reprenait ces esprits, \n Ceux-ci ce mirent à lui parler. \n Ces voix tantôt douces tantôt puissantes \n l'incitait à faire du mal à la nature autour de lui." ;
                     break;
                 case 4:
-				    texte.text = " l'incitait à faire du mal à la nature autour de lui \n Il trouva une torche face à lui";
+				texte.text = " Il trouva une torche face à lui.\n La flamme lui parla, le réchauffa, le réconforta  \n mais l'entraina par la même occassion\n dans la folie et dans la déchéance de son âme.";
                     break;
                 case 5:
-				    texte.text = "La flamme lui parla, le réchauffa, le réconforta  \n mais l'entraina par la même occassion dans la folie\n";
+				texte.text = "La natura lui sembla alors hostile :\n - Les lapin eurent tout à coup les yeux luisant. \n - Il pouvait voir des visages sur les fleurs.\n - Les canards avaient un comportement étrange. \n\n  TOUT CES ANIMAUX VOULAIENT SA MORT !";
                     break;
-                case 6:
-				    texte.text = "La natura lui sembla alors hostile \n Les lapin eurent tout à coup les yeux luisant \n il pouvait voir des visages sur les fleurs";
-                    break;
+
 			    case 7:
-				    texte.text = "Les canards volaient et pondaient des oeufs en même temps \n Tout ces animaux voulait sa mort";
+				    texte.text = "Il avait sombré dans la folie ! \n Mais ne l'était-il pas déjà avant ? \n \n Tout ce qu'il savait,\n c'est que la réponse se tennait entre ses mains. ";
 				    break;
 			    case 8:
-				    texte.text = "Il avait sombré dans la folie ! \n Mais ne l'était-il pas déjà avant ? \n out ce qu'il savait, c'est que la réponse se tennait entre ses mains. ";
-				    break;
-			    case 9:
-				texte.text = "Il comprit vite que le feu était l'unique solution à tout ces problèmes.\n Toutes les voix se mirent à chuchoter, murmurer, parler, crier, hurler la même chose ";
+				texte.text = "Il comprit vite que le feu était\n l'unique solution à tout ces problèmes.\n \n Toutes les voix se mirent à chuchoter, murmurer,\n parler, crier, hurler la même chose... ";
 				    break;			
-                 case 10:
+                 case 9:
 				    texte.text = "BURM'EM ALL.";
                     texte.fontSize = 45;
                     break;
-                case 11:
+                case 10:
 					SceneManager.LoadScene ("Level1");
                     break;
             }
